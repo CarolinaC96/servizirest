f1fddb706deceea71aeaeb4e5b34b0af265d1e3c
YangeBatolSt <yangesteve@gmail.com>
2022-01-12 10:53:16 +0100

    FB-FirstCommit

--- src/main/java/it/nttdata/servizirest/repository/JPARepository.java
+++ src/main/java/it/nttdata/servizirest/repository/JPARepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 //ogni volta che facciamo un nuovo repository lo aggiungiamo qui
 @Service
 @RequiredArgsConstructor
+
 public class JPARepository {
 
     private final ColourRepository colourRepository;
